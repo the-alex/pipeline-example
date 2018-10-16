426ea40cfff1a8680dd076c1d69bd2845d2c3ce4
Alex Chojnacki <thealex@umich.edu>
2018-10-15 21:59:03 -0400

    feat: Minimal results achieved!
    Take a look at those metrics!

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 #!/usr/bin/env python
 import utils
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+
+TARGET_LABEL = 'target'
 
 if __name__ == '__main__':
-    print(utils.foo())
+    print("-------------------- Fetch Data")
+    X, y = utils.fetch_data()
+    print("-------------------- Transform Data")
+    # Perform some feature transformation, like polynomialize
+    # Split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    print("-------------------- Train Model")
+    clf = RandomForestClassifier(n_estimators=100)
+    clf.fit(X_train, y_train)
+    print("-------------------- Evaluate Model")
+    y_hat = clf.predict(X_test)[:, np.newaxis]
+
+    zero_one_loss = np.sum(y_hat == y_test)
+    accuracy = np.mean(y_hat == y_test)
+    print("0-1 Loss: {}".format(zero_one_loss))
+    print("Accuracy: {}".format(accuracy))
+    print("-------------------- Memoize Experiment")
+    # persist with joblib
