9cc88544f1320999d73ca10e6cf0ae9a78b01dd9
Alex Chojnacki <thealex@umich.edu>
2018-10-15 21:58:17 -0400

    feat: Add fetch_data function
    Steals implementation from SKLearn's `make_classification`.

--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
 """
-This is the docstring
+A collection of utility functions to help ease the repetition in notebooks and
+model scripts.
 """
+import pandas as pd
+import numpy as np
+from sklearn.datasets import make_classification
 
-def foo():
-    return "bar"
+def fetch_data():
+    X, y = make_classification(n_samples=2000, n_features=10)
+    return pd.DataFrame(X), pd.DataFrame(y)
