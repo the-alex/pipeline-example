8fe49f7aa2d14d48171354e0b609bb5f02c046ea
Alex Chojnacki <thealex@umich.edu>
2018-10-16 20:58:16 -0400

    feat: Wrap classifier in randomized search /w CV
    This simply wraps the random forest with a random search that tests a
    distribution of parameters.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import utils
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
 
 random_state = 42
 np.random.seed(random_state)
@@ -18,12 +18,20 @@ if __name__ == '__main__':
     print("-------------------- Transform Data")
 
     # Perform some feature transformation, like polynomialize
+    X = utils.transform(X)
+
     # Split
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
     print("-------------------- Train Model")
 
-    clf = RandomForestClassifier(n_estimators=100)
+    clf = RandomizedSearchCV(
+        estimator=RandomForestClassifier(n_jobs=2),
+        param_distributions=dict(
+            n_estimators=[10, 100, 250],
+            max_depth=[3, 7, 10],
+        ),
+    )
     clf.fit(X_train, y_train)
 
     print("-------------------- Evaluate Model")
