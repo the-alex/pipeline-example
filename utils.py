f0be71e965744f34ea01fa22e6c6797218d9fc63
Alex Chojnacki <thealex@umich.edu>
2018-10-16 20:59:19 -0400

    feat: implement transform function PCA step
    This function fits a PCA model to the data and concatenates the
    transformed data to the existing dataframe.

--- utils.py
+++ utils.py
@@ -6,11 +6,21 @@ import pandas as pd
 import numpy as np
 from sklearn.datasets import make_classification
 from sklearn.metrics import roc_auc_score, accuracy_score, f1_score
+from sklearn.decomposition import PCA
 
 
 def fetch_data():
     X, y = make_classification(n_samples=2000, n_features=10)
     return pd.DataFrame(X), pd.DataFrame(y)
+
+
+def transform(X, transformers=dict()):
+    pca = transformers.get('pca')
+    if not pca:
+        pca = PCA(n_components=3)
+        pca.fit(X)
+    X = pd.concat([X, pd.DataFrame(pca.transform(X))], axis=1)
+    return X
 
 
 def make_report(y_pred, y_probs, y_true):
