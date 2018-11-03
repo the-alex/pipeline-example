d67e04434688e0f9e506f05ae4a654d506ce5dd8
Alex Chojnacki <thealex@umich.edu>
2018-11-03 02:47:20 -0400

    feat: Report sample size and class mean for report
    Reporting reports some basic stuff neglected the first time through.

--- reporting.py
+++ reporting.py
@@ -7,6 +7,8 @@ from sklearn.metrics import roc_auc_score, accuracy_score, f1_score
 
 def make_report(y_pred, y_probs, y_true):
     report = dict(
+        n=len(y_true),
+        class_mean=y_true.mean(),
         accuracy=accuracy_score(y_true, y_pred),
         auroc=roc_auc_score(y_true, y_probs),
         f1_score=f1_score(y_true, y_pred),
@@ -19,4 +21,6 @@ def pretty_print_report(report):
 Accuracy: {accuracy}
 AUROC   : {auroc}
 F1      : {f1_score}
+N       : {n}
+class % : {class_mean}
 """.format(**report)
